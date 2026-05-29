dca5cde48a35ceef4071072df375c769ddf7d2da
zainurdavi05 <zainurdavi05@gmail.com>
2026-05-29 10:40:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+// 1. Variabel input nilai utama
+let nilai = 80;
+let grade, status, predikat;
+
+// 2. Rumus logika percabangan untuk menentukan hasil otomatis
+if (nilai >= 85) {
+    grade = "A";
+    status = "Lulus";
+    predikat = "Sangat Memuaskan";
+} else if (nilai >= 75) {
+    grade = "B";
+    status = "Lulus";
+    predikat = "Memuaskan";
+} else if (nilai >= 60) {
+    grade = "C";
+    status = "Lulus";
+    predikat = "Cukup";
+} else {
+    grade = "E";
+    status = "Tidak Lulus";
+    predikat = "Kurang";
+}
+
+// 3. Mencetak hasil ke Console sesuai format gambar tugas
+console.log("Nilai anda :" + nilai);
+console.log("Grade :" + grade);
+console.log("Status :" + status);
+console.log("Predikat :" + predikat);
 
